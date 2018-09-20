8df0e1f8dc9d8fc07bc2b27cae07d231817aee5d
Veselin Minchev <veselin.minchev@gmail.com>
2018-09-20 16:25:49 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+// Counter Up Amimation
+
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+
+
+// Scroll Animation
+
+	$('a[href*="#"]')  
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
+
+
+
+
+// Fade out showcase containter
+
+$(window).scroll(function(){
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 300);
+  });
+
+
+
